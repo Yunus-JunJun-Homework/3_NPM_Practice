fe3b949394fb0e3626a1baa4a151ee1e485ee730
Yunus <seytnazarov.yunus@gmail.com>
2022-07-09 02:34:04 +0400

    NPM_Practice

---
+++ index.js
@@ -0,0 +1,13 @@
+const multiplication = (a, b) => {
+  return a * b;
+};
+const subtraction = (a, b) => {
+  return a - b;
+};
+const summation = (a, b) => {
+  return a + b;
+};
+
+console.log(multiplication(2, 2));
+console.log(subtraction(4, 2));
+console.log(summation(2, 2));
